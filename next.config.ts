862f4e7529690bf7985ee211adf029f32aeae303
BALASKANTHAN AK <balaslearn@gmail.com>
2025-10-16 08:02:01 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,20 +6,28 @@
 
 // export default nextConfig;
 
-import type { NextConfig } from "next";
 import js from "@eslint/js";
 import nextPlugin from "@next/eslint-plugin-next";
-import tseslint from "typescript-eslint";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
-export default tseslint.config(
+export default [
   js.configs.recommended,
-  ...tseslint.configs.recommended,
   nextPlugin.configs["core-web-vitals"],
   {
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
     rules: {
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off",
-      "react-hooks/exhaustive-deps": "off"
+      "react-hooks/exhaustive-deps": "off",
     },
-  }
-);
+  },
+];
